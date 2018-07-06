1a19b02e6f54b87c4621c60995d2411465ff797c
Henry O'Brien <h.obrien17@hotmail.com>
2018-07-06 15:37:19 +0800

    subprocess implementation

--- app/src/main/java/com/ei8htideas/acquanym/background/Subprocess.java
+++ app/src/main/java/com/ei8htideas/acquanym/background/Subprocess.java
@@ -1,19 +1,39 @@
 package com.ei8htideas.acquanym.background;
 
-import android.app.IntentService;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
 
 /**
  * Created by Henry on 6/07/2018.
  */
 
-public class Subprocess extends IntentService {
-    public Subprocess() {
-        super("Subprocess");
+public class Subprocess extends Service {
+
+    private final static int INTERVAL = 1000*60*2;
+    private Handler handler = new Handler();
+    private Runnable handlerTask = new Runnable() {
+        public void run() {
+            Log.i("Subprocess", "yay");
+            handler.postDelayed(handlerTask, INTERVAL);
+        }
+    };
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 
     @Override
-    protected void onHandleIntent(Intent workIntent) {
-        System.out.println("memes");
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        handlerTask.run();
+        return Service.START_STICKY;
     }
 }
