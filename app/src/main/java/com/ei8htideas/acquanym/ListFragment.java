a7c1b02e192f68a80c7175582a2b7e85103368ff
LAPTOP-HG38A5O2\adro_ <adrian.van.katwyk@gmail.com>
2018-07-09 17:29:24 +1000

    edited main and list fragment

--- app/src/main/java/com/ei8htideas/acquanym/ListFragment.java
+++ app/src/main/java/com/ei8htideas/acquanym/ListFragment.java
@@ -6,18 +6,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+
+import com.ei8htideas.acquanym.R;
 
 /**
  * Created by Henry on 4/07/2018.
  */
 
-public class MyFragment extends Fragment {
+public class ListFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //returning our layout file
         //change R.layout.yourlayoutfilename for each of your fragments
-        return inflater.inflate(R.layout.my_fragment, container, false);
+        return inflater.inflate(R.layout.list_fragment, container, false);
     }
 
     @Override
