41cbde2345e918fbd523ae36dfc9a88028595d76
Archit Sharma <architsharma97@hotmail.com>
2018-07-10 10:49:39 +1000

    More Functions added in Sign in UI and SDK added

--- app/src/main/java/com/ei8htideas/acquanym/sign_in.java
+++ app/src/main/java/com/ei8htideas/acquanym/sign_in.java
@@ -1,4 +1,29 @@
 package com.ei8htideas.acquanym;
 
-public class sign_in {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Archit on 9/07/2018.
+ */
+
+public class sign_in extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //returning our layout file
+        //change R.layout.yourlayoutfilename for each of your fragments
+        return inflater.inflate(R.layout.sign_in, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Sign in");
+    }
 }
